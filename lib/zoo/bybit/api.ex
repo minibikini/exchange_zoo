a02de4adb5468067aeecdc15348f2468cfe8933e
Bradford Folkens <i.am@bradfolkens.com>
2023-06-08 10:07:32 -0500

    Added more endpoints

--- lib/zoo/bybit/api.ex
+++ lib/zoo/bybit/api.ex
@@ -4,5 +4,9 @@ defmodule ExchangeZoo.Bybit.API do
   alias ExchangeZoo.Bybit.Model
 
   public :get, "/v5/market/instruments-info", Model.InstrumentsInfo
+  private :get, "/v5/order/realtime", Model.OrderResponse, as: :get_open_orders
   private :post, "/v5/order/create", Model.OrderResponse, as: :create_order
+  private :post, "/v5/order/create-batch", Model.OrderResponse, as: :create_batch_order # 404??
+  private :post, "/v5/order/cancel-batch", Model.OrderResponse, as: :cancel_batch_order # 404??
+  private :post, "/v5/order/cancel-all", Model.OrderResponse, as: :cancel_all_orders
 end
